3c2d8d052274801b04184db2ecf4c358b96d29ce
sruthinandula <83817949+sruthinandula@users.noreply.github.com>
2021-05-25 14:16:57 +0530

    Add files via upload

---
+++ Assignment 4.py
@@ -0,0 +1,58 @@
+
+import time
+import sys
+import ibmiotf.application
+import ibmiotf.device
+import random
+import json
+
+#Provide your IBM Watson Device Credentials
+organization = "178vg8"
+deviceType = "IOTdevice"
+deviceId = "1001"
+authMethod = "token"
+authToken = "1234567890"
+
+
+
+
+def myCommandCallback(cmd):
+        print("Command received: %s" % cmd.data['command'])
+
+
+        if cmd.data['command']=='lighton':
+                print("LIGHT ON IS RECEIVED")
+                
+                
+        elif cmd.data['command']=='lightoff':
+                print("LIGHT OFF IS RECEIVED")
+        
+        if cmd.command == "setInterval":
+                if 'interval' not in cmd.data:
+                        print("Error - command is missing required information: 'interval'")
+                else:
+                        interval = cmd.data['interval']
+        elif cmd.command == "print":
+                if 'message' not in cmd.data:
+                        print("Error - command is missing required information: 'message'")
+                else:
+                        print(cmd.data['message'])
+
+try:
+	deviceOptions = {"org": organization, "type": deviceType, "id": deviceId, "auth-method": authMethod, "auth-token": authToken}
+	deviceCli = ibmiotf.device.Client(deviceOptions)
+	#..............................................
+	
+except Exception as e:
+	print("Caught exception connecting device: %s" % str(e))
+	sys.exit()
+
+# Connect and send a datapoint "hello" with value "world" into the cloud as an event of type "greeting" 10 times
+deviceCli.connect()
+
+while True:
+        deviceCli.commandCallback = myCommandCallback
+
+# Disconnect the device and application from the cloud
+deviceCli.disconnect()
+
